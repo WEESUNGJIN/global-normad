00f55df68e6de4bccc1473d87c3949ab17f9ee00
Heesu <nizoo0516@gmail.com>
2025-10-14 13:57:28 +0900

    Chore: 사용되지 않는 코드 정리

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
 import backgroundSky from "@/assets/img/background_sky.png";
 import GNB from "@/components/GNB";
 import HeroSection from "@/components/main/HeroSection";
