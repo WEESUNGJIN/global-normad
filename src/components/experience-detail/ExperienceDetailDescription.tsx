90aaa9a44e001328529988e0873f32dc113f135f
Heesu <nizoo0516@gmail.com>
2025-10-19 20:33:19 +0900

    Design: 체험 상세 페이지 체험 설명 섹션 태블릿 반응형 디자인 구현

--- src/components/experience-detail/ExperienceDetailDescription.tsx
+++ src/components/experience-detail/ExperienceDetailDescription.tsx
@@ -3,9 +3,11 @@
 export default function ExperienceDetailDescription() {
   return (
     <section>
-      <div className="py-5 border-b border-gray-100">
-        <h2 className="typo-16-b text-gray-950 mb-2">체험 설명</h2>
-        <span className="typo-14-m text-gray-950">
+      <div className="py-5 md:py-8 border-b border-gray-100">
+        <h2 className="typo-16-b md:text-lg text-gray-950 mb-2 md:mb-3">
+          체험 설명
+        </h2>
+        <span className="typo-14-m !leading-[1.7] md:text-base text-gray-950">
           안녕하세요! 저희 스트릿 댄스 체험을 소개합니다. 저희는 신나고 재미있는
           스트릿 댄스 스타일을 가르칩니다. 크럼프는 세계적으로 인기 있는 댄스
           스타일로, 어디서든 춤출 수 있습니다. 저희 체험에서는 새로운 스타일을
