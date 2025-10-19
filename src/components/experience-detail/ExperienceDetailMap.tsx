8616f6ef4751da9a7f98760e8698b1819f0b4c91
Heesu <nizoo0516@gmail.com>
2025-10-19 20:40:27 +0900

    Design: 체험 상세 페이지 지도 섹션 태블릿 반응형 디자인 구현

--- src/components/experience-detail/ExperienceDetailMap.tsx
+++ src/components/experience-detail/ExperienceDetailMap.tsx
@@ -5,15 +5,14 @@ import Image from "next/image";
 export default function ExperienceDetailMap() {
   return (
     <section>
-      <div className="py-5 border-b border-gray-100">
-        <h2 className="typo-16-b text-gray-950 mb-2">오시는 길</h2>
-
-        <p className="typo-14-sb text-gray-700 mb-2">
+      <div className="py-5 md:py-8 border-b border-gray-100">
+        <h2 className="typo-16-b md:text-lg text-gray-950 mb-2">오시는 길</h2>
+        <p className="typo-14-sb md:text-base text-gray-700 mb-2 md:mb-3">
           서울 중구 청계천로 100 10F (스트릿댄스 스튜디오)
         </p>
 
         {/* 지도 API 삽입*/}
-        <div className="w-full h-[240px] rounded-2xl overflow-hidden bg-gray-100">
+        <div className="w-full h-[23vh] md:h-[40vh] rounded-2xl overflow-hidden bg-gray-100">
           {/* 카카오 지도나 네이버 지도 iframe 삽입 예정 */}
         </div>
       </div>
