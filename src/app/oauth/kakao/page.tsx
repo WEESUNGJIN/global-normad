7db1365bfa8f31a72dbffad4d243281872fa55e1
Ucastle <skaska007@naver.com>
2025-10-25 23:28:31 +0900

    kakao/page.tsx 정적 최적화

--- src/app/oauth/kakao/page.tsx
+++ src/app/oauth/kakao/page.tsx
@@ -7,6 +7,8 @@ import KakaoSignupHandler from "./KakaoSignupHandler";
 import LoadingSpinner from "@/components/auth-detail/LoadingSpinner";
 
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const fetchCache = "force-no-store";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function KakaoSignupPage({ searchParams }: any) {
