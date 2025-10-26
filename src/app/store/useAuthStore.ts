64225ccf86ebd1f9ce82090ca779ce1fb53ae2cc
Ucastle <skaska007@naver.com>
2025-10-27 05:31:33 +0900

    로그아웃시 persist 데이터까지 제거

--- src/app/store/useAuthStore.ts
+++ src/app/store/useAuthStore.ts
@@ -30,6 +30,7 @@ export const useAuthStore = create(
       logout: () => {
         localStorage.removeItem("accessToken");
         localStorage.removeItem("refreshToken");
+        localStorage.removeItem("auth-storage");
         set({ user: null, isAuthenticated: false, isResoring: false });
       },
     }),
